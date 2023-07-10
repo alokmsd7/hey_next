675c80c89e5285b85d7abbe7e585129ab05c54ae
alokmsd7 <74170420+alokmsd7@users.noreply.github.com>
2023-07-10 23:38:50 +0530

    Update route.tsx

--- app/api/topics/route.tsx
+++ app/api/topics/route.tsx
@@ -1,32 +1,23 @@
 import connectMongoDB from "@/libs/mongodb";
 import Topics from "@/models/topics";
-import { NextResponse, Request } from "next/server";
+import { NextApiResponse } from "next";
 
-export async function PUT(request: Request, { params }: any) {
-  const { id } = params;
-  const { newTitle: title, newDescription: description } = await request.json();
-  await connectMongoDB();
-  await Topics.findByIdAndUpdate(id, { title, description });
-  return NextResponse.json({ message: "Topic updated" }, { status: 200 });
-}
-
-export async function GET(request: Request, { params }: any) {
-  const { id } = params;
-  await connectMongoDB();
-  const topic = await Topics.findOne({ _id: id });
-  return NextResponse.json({ topic }, { status: 200 });
-}
-
-export async function POST(request: Request) {
+export async function POST(request: NextApiRequest) {
   const { title, description } = await request.json();
   await connectMongoDB();
   await Topics.create({ title, description });
-  return NextResponse.json({ message: "Topic Created" }, { status: 201 });
+  return NextApiResponse.json({ message: "Topic Created" }, { status: 201 });
+} 
+
+export async function GET() {
+  await connectMongoDB();
+  const topics = await Topics.find();
+  return NextApiResponse.json({ topics });
 }
 
-export async function DELETE(request: Request) {
-  const id = request.nextUrl.searchParams.get("id");
+export async function DELETE(request: NextApiRequest) {
+  const id = request.query.id as string;
   await connectMongoDB();
   await Topics.findByIdAndDelete(id);
-  return NextResponse.json({ message: "Topic deleted" }, { status: 200 });
+  return NextApiResponse.json({ message: "Topic deleted" }, { status: 200 });
 }
